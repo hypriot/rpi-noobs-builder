9ad53df2cb28ea6a3d8b4e3ee7e9ab53c78c6573
firecyberice <firecyberice@users.noreply.github.com>
2015-03-15 01:38:48 +0100

    Update create_noobs.sh

--- create_noobs.sh
+++ create_noobs.sh
@@ -8,12 +8,9 @@ path=`pwd`
 
 imagename=`cat VERSION`
 
-echo $REGION
-echo $AWS_REGION
+#echo $REGION
+#echo $AWS_REGION
 #aws s3 --region eu-central-1 cp s3://buildserver-production/images/$imagename $BUILD_INPUTS/
-
-
-modprobe loop
 
 aws s3 cp s3://buildserver-production/images/$imagename $BUILD_INPUTS/
 
